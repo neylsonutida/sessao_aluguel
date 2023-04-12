f039ab3232bebcb6ee2b584c36bad7a7902b9e92
neylsonutida <neylson.utida@gmail.com>
2023-04-11 20:27:07 -0400

    imagem alinhada

--- home.php
+++ home.php
@@ -5,20 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="imagem.css">
 </head>
 <body>
 <p>
     <h4>Deslocar-se para diferentes lugares com mais liberdade na cidade de CUIABÁ-MT, alugando bicicletas no 
     YoPedal há mais praticidade, afinal há diversas estações espalhadas pela cidade para você escolher com diferentes planos
-    para sua rotina.</h4><br><br>
+    para sua rotina. Pegue uma bicicleta e devolva em qualquer outra estação.</h4><br><br>
 </p>
 <p>
-    <h4>Confira os principais modelos de bike e o local das estações no Alugue sua bicicleta depois de 
-    ter logado para iniciar sua sessão.</h4>
+    <h4>Confira os principais modelos de bike e o local das estações no Alugue sua bicicleta.</h4>
 </p>
 
-<img src="https://bikeitau.com.br/wp-content/uploads/2021/05/bike-eletrica.webp" alt="">Bicicleta itaú elétrica<br>
-<img src="https://bikeitau.com.br/wp-content/uploads/2021/05/bike-normal.webp" alt="">Bicicleta itaú normal
-
+<div class="divimagem">
+    <div class="imagem1">
+    <img src="https://bikeitau.com.br/wp-content/uploads/2021/05/bike-eletrica.webp" alt="">
+    <figcaption>Bicicleta itaú elétrica</figcaption> 
+    </div>
+    <div class="imagem2">
+    <img src="https://bikeitau.com.br/wp-content/uploads/2021/05/bike-normal.webp" alt="">
+    <figcaption>Bicicleta itaú normal</figcaption>
+    </div>
+</div>
 </body>
 </html>
