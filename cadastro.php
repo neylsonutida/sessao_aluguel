57bb795d727a20f1a5b8472be97e2b368f57dc5e
neylsonutida <neylson.utida@gmail.com>
2023-05-04 21:59:08 -0400

    adicionei validação no formulario de cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
+
+
     session_start();
     $nome = $_POST["nome"];
     $cpf = $_POST["cpf"];
@@ -17,35 +10,59 @@
     $erro = FALSE;
     $_SESSION['pessoa'] = $nome;
 
+    if(!strstr($email, '@')){
+        echo "A senha deve conter @ <br><br>";
+        echo "<a href='cadastro.html'>Voltar para o Cadastro</a> <br><br>";
+        $erro = TRUE;
+    }
     if(empty($nome)){
-        echo "Digite o <strong> nome </strong> <br><br>";
+        echo "Digite o <strong> nome... </strong> <br><br>";
+        echo "<a href='cadastro.html'>Voltar para o Cadastro</a> <br><br>";
         $erro = TRUE;
     }
     if(empty($cpf)){
-        echo "Digite o <strong> CPF </strong> <br><br>";
+        echo "Digite o <strong> CPF... </strong> <br><br>";
+        echo "<a href='cadastro.html'>Voltar para o Cadastro</a> <br><br>";
         $erro = TRUE;
     }
     if(empty($email)){
-        echo "Digite o <strong> email </strong> <br><br>";
+        echo "Digite o <strong> email... </strong> <br><br>";
+        echo "<a href='cadastro.html'>Voltar para o Cadastro</a> <br><br>";
         $erro = TRUE;
     }
     if(!strstr($senha, $senha2)){
         echo "As senhas devem ser iguais!";
+        echo "<a href='cadastro.html'>Voltar para o Cadastro</a> <br><br>";
         $erro = TRUE;
     }
     if(strstr($senha, ' ')){
-        echo "A senha nao pode ter espaço<br><br>";
+        echo "A senha nao pode ter espaço...<br><br>";
+        echo "<a href='cadastro.html'>Voltar para o Cadastro</a> <br><br>";
         $erro = TRUE;
     }
     if(empty($senha)){
-        echo "Digite a senha <br><br>";
+        echo "Digite a senha... <br><br>";
+        echo "<a href='cadastro.html'>Voltar para o Cadastro</a> <br><br>";
         $erro = TRUE;
     }
     if(!$erro){
-        echo "Tudo certo!<br>";
-        echo "Cadastro realizado com sucesso<br>";
-        echo "<a href='login.html'>Continuar para o Login</a>";
+        include "conexao.php";
+    //instanciar objeto
+    //url, nome do banco, user, senha
+    //C:\xampp\phpMyAdmin\config.inc
+    //1- 
+    $sql = "INSERT INTO cadastro (nome, cpf, email, senha)
+            VALUES (:nome, :cpf, :email, :senha)";
+
+    //stmt = statement
+        $stmt = $conexao->prepare($sql);
+
+        $stmt->bindValue(':nome', $nome);
+        $stmt->bindValue(':email', $email);
+        $stmt->bindValue(':cpf', $cpf);
+        $stmt->bindValue(':senha', $senha);
+        $stmt->execute();
+        header("Location: login.html");
     }
     ?>
 </body>
-</html>
