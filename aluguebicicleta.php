804c2ea4036dc3d3c1c065075239b7893d9a3168
neylsonutida <neylson.utida@gmail.com>
2023-05-04 22:00:56 -0400

    Instanciei o objeto com banco

--- aluguebicicleta.php
+++ aluguebicicleta.php
@@ -1,13 +1,42 @@
 <?php
 session_start();
 
-    echo " ".$_SESSION['pessoa'].", bicicleta alugada com sucesso!!<br><br><hr>";
+    $bicicletas = $_GET['bicicletas'];
+    $tempo = $_GET['tempo'];
+    $local = $_GET['local'];
+    $erro = FALSE;
 
+    if(empty($local)){
+        echo "Escolha o <strong> local... </strong> <br><br>";
+        echo "<a href='aluguebicicleta.php'>Voltar para o Cadastro</a> <br><br>";
+        $erro = TRUE;
+    }
+    if(!$erro){
+    //instanciar objeto
+    //url, nome do banco, user, senha
+    //C:\xampp\phpMyAdmin\config.inc
+    //1- 
+        include "conexao.php";
 
+    //stmt = statement
+        $sql = "INSERT INTO aluguel (bicicletas, tempo, local)
+                VALUES (:bicicletas, :tempo, :local)";
 
-    echo "Tipo de bicicleta: " .$_GET['bicicletas']."<br>";
-    echo "Tempo de aluguel: " .$_GET['tempo']."<br>";
-    echo "Local: " .$_GET['local']."<br><br>";
+        $stmt = $conexao->prepare($sql);
 
-    echo "<a href='index.php'>Voltar para o Menu</a>";
+        $stmt->bindValue(':bicicletas', $bicicletas);
+        $stmt->bindValue(':tempo', $tempo);
+        $stmt->bindValue(':local', $local);
+        $stmt->execute();
+
+        echo " ".$_SESSION['pessoa'].", bicicleta alugada com sucesso!!<br><br><hr>";
+
+        echo "Tipo de bicicleta: " .$_GET['bicicletas']."<br>";
+        echo "Tempo de aluguel: " .$_GET['tempo']."<br>";
+        echo "Local: " .$_GET['local']."<br><br>";
+    
+        echo "<a href='index.php'>Voltar para o Menu</a>";
+    }
+
+    
 ?>
